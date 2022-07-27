9e820d8cbad85663ec348b894b1cb2a08c449ecd
BinbinLee <lbbxsxlz@gmail.com>
2022-07-27 13:28:01 +0800

    Update emu-function.c

--- emu-function.c
+++ emu-function.c
@@ -1,8 +1,33 @@
+libspdm_return_t do_handshake_via_spdm(void)
+{
+    void *spdm_context;
+    libspdm_return_t status;
+    uint32_t session_id;
+    uint8_t heartbeat_period;
+    uint8_t measurement_hash[LIBSPDM_MAX_HASH_SIZE];
+
+    spdm_context = m_spdm_context;
+    heartbeat_period = 0;
+    
+    libspdm_zero_mem(measurement_hash, sizeof(measurement_hash));
+    /* Only test KEY_EXCHANGE and FINISH request messages */
+    status = libspdm_start_session(spdm_context, false,
+                                   m_use_measurement_summary_hash_type,
+                                   m_use_slot_id, m_session_policy, &session_id,
+                                   &heartbeat_period, measurement_hash);
+    if (LIBSPDM_STATUS_IS_ERROR(status)) {
+        printf("libspdm_start_session - %x\n", (uint32_t)status);
+        return status;
+    }
+
+    return LIBSPDM_SEVERITY_SUCCESS;
+}
+
 /*
 * These function implements the request and response messages used for provisioning a device with certificate chains.
 * Provisioning of Slot 0 should be only done in a secure environment (such as a secure manufacturing environment)
 */
-libspdm_return_t do_certificate_provising_via_spdm(void)
+libspdm_return_t do_certificate_provising_via_spdm(uint32_t* session_id)
 {
     void *spdm_context;
 
@@ -21,67 +46,110 @@ libspdm_return_t do_certificate_provising_via_spdm(void)
     cert_chain_size_to_set = 0;
 #endif /*LIBSPDM_ENABLE_SET_CERTIFICATE_CAP*/
 
-    libspdm_return_t status = LIBSPDM_SEVERITY_SUCCESS;
+    libspdm_return_t status;
     spdm_context = m_spdm_context;
 
 //#if LIBSPDM_ENABLE_CAPABILITY_GET_CSR_CAP
-    if (m_use_version >= SPDM_MESSAGE_VERSION_12) {
-        /*get csr*/
-        csr_len = LIBSPDM_MAX_CSR_SIZE;
-        libspdm_zero_mem(csr_form_get, sizeof(csr_form_get));
-        if ((m_exe_session & EXE_SESSION_GET_CSR) != 0) {
-            status = libspdm_get_csr(spdm_context, NULL, 0, NULL, 0, NULL, csr_form_get,
-                                    &csr_len);
-            if (LIBSPDM_STATUS_IS_ERROR(status)) {
-                printf("libspdm_get_csr - %x\n",
-                        (uint32_t)status);
-                return status;
-            }
+
+    /*get csr*/
+    csr_len = LIBSPDM_MAX_CSR_SIZE;
+    libspdm_zero_mem(csr_form_get, sizeof(csr_form_get));
+    if ((m_exe_session & EXE_SESSION_GET_CSR) != 0) {
+        status = libspdm_get_csr(spdm_context, NULL, 0, NULL, 0, NULL, csr_form_get,
+                                &csr_len);
+        if (LIBSPDM_STATUS_IS_ERROR(status)) {
+            printf("libspdm_get_csr - %x\n",
+                    (uint32_t)status);
+            return status;
         }
-    }    
+    }
+ 
 //#endif /*LIBSPDM_ENABLE_CAPABILITY_GET_CSR_CAP*/
 
 #if LIBSPDM_ENABLE_SET_CERTIFICATE_CAP
-    if (m_use_version >= SPDM_MESSAGE_VERSION_12) {
-        res = libspdm_read_responder_public_certificate_chain(m_use_hash_algo,
-                                                            m_use_asym_algo,
-                                                            &cert_chain_to_set,
-                                                            &cert_chain_size_to_set,
-                                                            NULL, NULL);
-        if (!res) {
-            printf("set certificate :read_responder_public_certificate_chain fail!\n");
+    res = libspdm_read_responder_public_certificate_chain(m_use_hash_algo,
+                                                        m_use_asym_algo,
+                                                        &cert_chain_to_set,
+                                                        &cert_chain_size_to_set,
+                                                        NULL, NULL);
+    if (!res) {
+        printf("set certificate :read_responder_public_certificate_chain fail!\n");
+        free(cert_chain_to_set);
+        return LIBSPDM_STATUS_INVALID_CERT;
+    }
+
+    /*set_certificate for slot_id:0 in secure environment*/
+    if ((m_exe_session & EXE_SESSION_SET_CERT) != 0) {
+        slot_id = 0;
+        status = libspdm_set_certificate(spdm_context, slot_id,
+                                        cert_chain_to_set, cert_chain_size_to_set, NULL);
+
+        if (LIBSPDM_STATUS_IS_ERROR(status)) {
+            printf("libspdm_set_certificate - %x\n",
+                (uint32_t)status);
             free(cert_chain_to_set);
-            return LIBSPDM_STATUS_INVALID_CERT;
+            return status;
         }
 
-        /*set_certificate for slot_id:0 in secure environment*/
+    }
+
+    /*set_certificate for slot_id:1 in secure session*/
+    if (session_id != NULL) {
         if ((m_exe_session & EXE_SESSION_SET_CERT) != 0) {
-            slot_id = 0;
+            slot_id = 1;
             status = libspdm_set_certificate(spdm_context, slot_id,
-                                            cert_chain_to_set, cert_chain_size_to_set, NULL);
+                                            cert_chain_to_set, cert_chain_size_to_set, session_id);
 
             if (LIBSPDM_STATUS_IS_ERROR(status)) {
                 printf("libspdm_set_certificate - %x\n",
-                        (uint32_t)status);
+                            (uint32_t)status);
             }
 
+            free(cert_chain_to_set);
+            return status;
         }
-
-        free(cert_chain_to_set);
     }
+
+    free(cert_chain_to_set);
 #endif /*LIBSPDM_ENABLE_SET_CERTIFICATE_CAP*/ 
-    return status;  
+    return LIBSPDM_SEVERITY_SUCCESS;  
 }
+
 
 
 /* when use --trans NONE, skip secure session  */
     if (m_use_transport_layer == SOCKET_TRANSPORT_TYPE_NONE) {
-        status = do_certificate_provising_via_spdm();
-        if (LIBSPDM_STATUS_IS_ERROR(status)) {
-                printf("do_session_via_spdm - %x\n",
-                       (uint32_t)status);
-            goto done;
+        if (m_use_version >= SPDM_MESSAGE_VERSION_11) {
+            if ((m_exe_session & EXE_SESSION_KEY_EX) != 0) {
+                status = do_handshake_via_spdm();
+                if (LIBSPDM_STATUS_IS_ERROR(status)) {
+                    printf("do_handshake_via_spdm - %x\n",
+                        (uint32_t)status);
+                    goto done;
+                }
+            }
+
+            if ((m_exe_session & EXE_SESSION_KEY_EX) != 0) {
+                if (m_use_slot_id == 0) {
+                    m_use_slot_id =  1;
+                    status = do_handshake_via_spdm();
+                    if (LIBSPDM_STATUS_IS_ERROR(status)) {
+                        printf("do_handshake_via_spdm - %x\n",
+                            (uint32_t)status);
+                        goto done;
+                    }
+                }
+            }
+        }
+
+        if (m_use_version >= SPDM_MESSAGE_VERSION_12) {      
+            status = do_certificate_provising_via_spdm(NULL);
+            if (LIBSPDM_STATUS_IS_ERROR(status)) {
+                    printf("do_certificate_provising_via_spdm - %x\n",
+                        (uint32_t)status);
+                goto done;
+            }
         }
     }
-   else {
-   }
+    else {
+    }
