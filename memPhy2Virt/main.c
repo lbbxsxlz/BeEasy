6dc24d48fecafe1cb9a8b5eb31bc89885c9b2757
BinbinLee <lbbxsxlz@gmail.com>
2022-09-08 23:02:40 +0800

    update command arguments

--- memPhy2Virt/main.c
+++ memPhy2Virt/main.c
@@ -7,15 +7,15 @@
 
 static void usage(char *path)
 {
-        fprintf(stderr, "%s [ -l <len> ] [ -j <journal size> ] [ -b <block_size> ]\n", basename(path));
-        fprintf(stderr, "    [ -g <blocks per group> ] [ -i <inodes> ] [ -I <inode size> ]\n");
-        fprintf(stderr, "    [ -L <label> ] [ -f ] [ -a <android mountpoint> ]\n");
-        fprintf(stderr, "    [ -z | -s ] [ -J ]\n");
-        fprintf(stderr, "    <filename> [<directory>]\n");
+	fprintf(stderr, "\nInvalid parameters! \n");
+	fprintf(stderr, "Please input the correct parmeters! The correct samples are on below: \n\n");
+	fprintf(stderr, "%s -r -a $address \n", basename(path));
+	fprintf(stderr, "%s -s -a $address -l $length \n", basename(path));
+	fprintf(stderr, "%s -w -a $address -v $value \n", basename(path));
 }
 
 /* base16 to base10 */
-int string2dec(void *nptr)
+unsigned long string2dec(void *nptr)
 {
 	char *p = (char *)nptr;
 
@@ -36,19 +36,42 @@ int string2dec(void *nptr)
 int main(int argc, char* argv[])
 {
 	int opt;
+	char mode[10] = {0};
+	unsigned length;
+	unsigned addr;
+	unsigned value;
 
-	while ((opt = getopt(argc, argv, "h:m")) != -1) {
+	while ((opt = getopt(argc, argv, "a:l:v:hrsw")) != -1) {
         switch (opt) {
         case 'h':
-                usage(argv[0]);
-                break;
-        case 'm':
-                break;
+            usage(argv[0]);
+            break;
+		case 'a':
+			addr = string2dec(optarg);
+			break;
+		case 'l':
+			length = string2dec(optarg);
+			break;
+		case 'v':
+			value = string2dec(optarg);
+			break;
+        case 'r':
+			strncpy(mode, "read", sizeof(mode) -1);
+            break;
+		case 's':
+			strncpy(mode, "show", sizeof(mode) -1);
+            break;	
+		case 'w':
+			strncpy(mode, "write", sizeof(mode) -1);
+            break;	
+			
         default: 
 			usage(argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
+	
+	
 	return 0;
 }
 
