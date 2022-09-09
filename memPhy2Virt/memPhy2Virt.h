1474a2ddb388d1151f0bb96627f45af9be9baed2
BinbinLee <lbbxsxlz@gmail.com>
2022-09-09 15:03:42 +0800

    add new function decrement

--- memPhy2Virt/memPhy2Virt.h
+++ memPhy2Virt/memPhy2Virt.h
@@ -4,7 +4,8 @@
 #define PAGE_SIZE 0x1000
 #define PAGE_SIZE_MASK 0xfffffffffffff000
 
-int showRegValue(unsigned long phyAddr, unsigned len);
+int showRegValue(unsigned long phyAddr, unsigned long len);
 int writeReg(unsigned long phyAddr, unsigned long value);
+int readReg(unsigned long phyAddr, unsigned long *value);
 
 #endif
