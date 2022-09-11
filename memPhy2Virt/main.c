52bfd80985263471ab32f561e0ea2d3b595e901f
BinbinLee <lbbxsxlz@gmail.com>
2022-09-11 22:29:21 +0800

    update code

--- memPhy2Virt/main.c
+++ memPhy2Virt/main.c
@@ -4,23 +4,22 @@
 #include <string.h>
 #include <libgen.h>
 #include <getopt.h>
+#include "memPhy2Virt.h"
 
 static void usage(char *path)
 {
-	fprintf(stderr, "\nInvalid parameters! \n");
-	fprintf(stderr, "Please input the correct parmeters! The correct samples are on below: \n\n");
+	fprintf(stderr, "The samples are on below: \n\n");
 	fprintf(stderr, "%s -r -a $address \n", basename(path));
 	fprintf(stderr, "%s -s -a $address -l $length \n", basename(path));
 	fprintf(stderr, "%s -w -a $address -v $value \n", basename(path));
 }
 
-/* base16 to base10 */
 unsigned long string2dec(void *nptr)
 {
 	char *p = (char *)nptr;
 
-    if (NULL == p)
-    	return -1;
+	if (NULL == p)
+		return -1;
 
 	if(strncmp("0x", p, 2)) {
 		return strtoul(p, NULL, 0);
@@ -36,16 +35,17 @@ unsigned long string2dec(void *nptr)
 int main(int argc, char* argv[])
 {
 	int opt;
+	int ret;
 	char mode[10] = {0};
-	unsigned length;
-	unsigned addr;
-	unsigned value;
+	unsigned long length = 0;
+	unsigned long addr = 0;
+	unsigned long value = 0;
 
 	while ((opt = getopt(argc, argv, "a:l:v:hrsw")) != -1) {
-        switch (opt) {
-        case 'h':
-            usage(argv[0]);
-            break;
+		switch (opt) {
+		case 'h':
+			usage(argv[0]);
+			break;
 		case 'a':
 			addr = string2dec(optarg);
 			break;
@@ -55,23 +55,69 @@ int main(int argc, char* argv[])
 		case 'v':
 			value = string2dec(optarg);
 			break;
-        case 'r':
+		case 'r':
 			strncpy(mode, "read", sizeof(mode) -1);
-            break;
+		break;
 		case 's':
 			strncpy(mode, "show", sizeof(mode) -1);
-            break;	
+			break;	
 		case 'w':
 			strncpy(mode, "write", sizeof(mode) -1);
-            break;	
+			break;	
 			
-        default: 
+		default:
+			fprintf(stderr, "\nInvalid parameters! \n");
+			fprintf(stderr, "Please input the correct parmeters!"); 
 			usage(argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
 	
+	if (strncmp(mode, "read", 4) == 0) {
+		if (addr == 0) {
+			fprintf(stderr, "\nInvalid parameters! \n");
+			usage(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		else {
+			ret = readReg(addr, &value);
+			if (ret < 0) {
+				printf("read Reg[0x%lx] fail \n", addr);
+				exit(EXIT_FAILURE);
+			}
+			printf("Reg[0x%lx] = 0x%lx \n", addr, value);
+		}
+	} else if (strncmp(mode, "write", 5) == 0) {
+		if (addr == 0 || value == 0) {
+			fprintf(stderr, "\nInvalid parameters! \n");
+			usage(argv[0]);
+			exit(EXIT_FAILURE);
+		} else {
+			ret = writeReg(addr, value);
+			if (ret < 0) {
+				printf("write Reg[0x%lx] fail \n", addr);
+				exit(EXIT_FAILURE);
+			}
+		}	
+	} else if (strncmp(mode, "show", 4) == 0) {
+		if (addr == 0 || length == 0) {
+			fprintf(stderr, "\nInvalid parameters! \n");
+			usage(argv[0]);
+			exit(EXIT_FAILURE);
+		} else {
+			ret = showRegValue(addr, length);
+			if (ret < 0) {
+				printf("show Reg[0x%lx] fail \n", addr);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	else {
+		fprintf(stderr, "\nInvalid parameters! \n");
+		fprintf(stderr, "Please input the correct parmeters!"); 
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	
 	return 0;
 }
-
