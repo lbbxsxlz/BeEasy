6a1cd201ab9a982a2aadeb1ecb0ec9a2044757ee
BinbinLee <lbbxsxlz@gmail.com>
2022-10-19 21:34:46 +0800

    add write function

--- mii/mii.c
+++ mii/mii.c
@@ -10,76 +10,126 @@
 #include <linux/types.h>
 #include <netinet/in.h>
 
-#define OSA_EFAIL -1
-#define OSA_SOK 0
+#define PHY_ID_HIGH_WORD 2
+#define PHY_ID_LOW_ORD 3
 	
 static int readPhyReg(const char* ifname, __u16 regNum, __u16* val)
 {
 	struct mii_ioctl_data *mii = NULL;
-    struct ifreq ifr;
-    int ret;
-    int sockfd = -1;
+	struct ifreq ifr;
+	int ret;
+	int sockfd = -1;
 
-    if (ifname == NULL)
-    {
+	if (ifname == NULL)
+	{
 		printf("invalid ifname \n");
-		return OSA_EFAIL;
-    }
+		return -1;
+	}
 
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+	memset(&ifr, 0, sizeof(ifr));
+	strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
 
-    sockfd = socket(PF_LOCAL, SOCK_DGRAM, 0);
-    if (0 > sockfd)
-    {
+	sockfd = socket(PF_LOCAL, SOCK_DGRAM, 0);
+	if (0 > sockfd)
+	{
 		printf("create socket fail \n");
-		return OSA_EFAIL;
-    }
+		return -1;
+	}
 
 	/* get phy address */
-    ret = ioctl(sockfd, SIOCGMIIPHY, &ifr);
+	ret = ioctl(sockfd, SIOCGMIIPHY, &ifr);
 	if (0 > ret)
-    {
+	{
 		printf("get phy address fail \n");
 		close(sockfd);
-		return OSA_EFAIL;
-    }
+		return -1;
+	}
 
-    mii = (struct mii_ioctl_data*)&ifr.ifr_data;
-    mii->reg_num = regNum;
+	mii = (struct mii_ioctl_data*)&ifr.ifr_data;
+	mii->reg_num = regNum;
 
-    ret = ioctl(sockfd, SIOCGMIIREG, &ifr);
-    if (0 > ret)
-    {
+	ret = ioctl(sockfd, SIOCGMIIREG, &ifr);
+	if (0 > ret)
+	{
 		printf("get phy register fail \n");
 		close(sockfd);
-		return OSA_EFAIL;
-    }
+		return -1;
+	}
 
-    *val = mii->val_out;
+	*val = mii->val_out;
 	close(sockfd);
-    return OSA_SOK;
+	return 0;
 }
+
+static int writePhyReg(const char* ifname, __u16 regNum, __u16 val)
+{
+	struct mii_ioctl_data *mii = NULL;
+	struct ifreq ifr;
+	int ret;
+	int sockfd = -1;
+
+	if (ifname == NULL)
+	{
+		printf("invalid ifname \n");
+		return -1;
+	}
+
+	memset(&ifr, 0, sizeof(ifr));
+	strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+
+	sockfd = socket(PF_LOCAL, SOCK_DGRAM, 0);
+	if (0 > sockfd)
+	{
+		printf("create socket fail \n");
+		return -1;
+	}
+
+	/* get phy address */
+	ret = ioctl(sockfd, SIOCGMIIPHY, &ifr);
+	if (0 > ret)
+	{
+		printf("get phy address fail \n");
+		close(sockfd);
+		return -1;
+	}
+
+	mii = (struct mii_ioctl_data*)&ifr.ifr_data;
+	mii->reg_num = regNum;
+	mii->val_in = val;
+
+	ret = ioctl(sockfd, SIOCSMIIREG, &ifr);
+	if (0 > ret)
+	{
+		printf("get phy register fail \n");
+		close(sockfd);
+		return -1;
+	}
+	
+	close(sockfd);
+	return 0;
+}
+
+
 static int getPhyID(const char* ifname, uint32_t* id)
 {
-    __u16 reg2;
-    __u16 reg3;
+	__u16 reg2;
+	__u16 reg3;
 
 	int ret;
 	/* 获取phy寄存器2 */
-	ret = readPhyReg(ifname, 2, &reg2);
+	ret = readPhyReg(ifname, PHY_ID_HIGH_WORD, &reg2);
 	if (0 > ret)
 	{
 		printf("get phy reg2 fail \n");
-		return OSA_EFAIL;
+		return -1;
 	}
 
-    /* 获取phy寄存器3 */
-	ret = readPhyReg(ifname, 3, &reg3);
+	/* 获取phy寄存器3 */
+	ret = readPhyReg(ifname, PHY_ID_LOW_WORD, &reg3);
 	if (0 > ret)
 	{
 		printf("get phy reg3 fail \n");
-		return OSA_EFAIL;
+		return -1;
 	}
 
 	*id = reg2 << 16 | reg3;
